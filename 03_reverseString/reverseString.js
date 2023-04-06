f262268c5c81c34f74107d100798526d5ea81838
Aditya-Naresh <adityanaresh007@gmail.com>
2023-04-06 23:26:27 +0530

    Managed to do 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let i = string.length -1
+    let ans =""
 
+    while(i > -1){
+        ans += string[i];
+        i-- 
+    }
+
+    return ans;
 };
 
 // Do not edit below this line
