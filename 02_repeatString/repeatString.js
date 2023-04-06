3721d446eae8158f0615b59324f0b752b8513e00
Aditya-Naresh <adityanaresh007@gmail.com>
2023-04-06 22:32:43 +0530

    Managed to do 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if(num < 0){
+        return ('ERROR')
+    }
+    let sol = "";
 
+    for(let i = 0 ; i < num; i++){
+        sol = sol+str;
+    }
+    return sol;
 };
 
 // Do not edit below this line
