a91ec4d007a0f6c2861d812eeaad9b5973c6f56a
Aditya-Naresh <adityanaresh007@gmail.com>
2023-04-07 18:01:05 +0530

    Managed to do 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  result = 0
+	for(let i = 0; i < nums.length; i++){
+    result += nums[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  result = 1
+	for(let i = 0; i < nums.length; i++){
+    result *= nums[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(n ,e) {
+	return n ** e 
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n === 0){
+    return 1;
+  }
+
+  return n * factorial(n-1);
 };
 
 // Do not edit below this line
