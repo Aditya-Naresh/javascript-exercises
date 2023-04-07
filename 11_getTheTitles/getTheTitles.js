8c55b4cc5a89dc084cfb6b325256d08ab6af520c
Aditya-Naresh <adityanaresh007@gmail.com>
2023-04-07 21:43:52 +0530

    Managed to do 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    let titleArray = []
+    for (let i = 0; i < obj.length; i++) {
+        titleArray[i] = obj[i].title
+        
+    }
+    return titleArray;
 };
 
 // Do not edit below this line
