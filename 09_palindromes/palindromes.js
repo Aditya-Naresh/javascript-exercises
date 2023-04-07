1972c632f9406093c4743a32a6f774a0478e931c
Aditya-Naresh <adityanaresh007@gmail.com>
2023-04-07 18:12:28 +0530

    Managed to do 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^\w]|_/g, "")
+    for(let i = 0, j = str.length-1; i < j; i++, j--){
+        if(str[i] !== str[j]){
+            return false;
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
